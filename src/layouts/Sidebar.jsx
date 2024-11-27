d317716fe75a9024e76505f8a019fe3a08baf97d
Anwarul Haque <anwarul@b2cinfosolutions.com>
2024-11-27 15:55:53 +0530

    side menu active

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
   const location = useLocation(); // Get the current route path
 
   // Helper function to check if the path matches
-  const isActive = (path) => location.pathname === path;
+  const isActive = (path) => location.pathname.startsWith(path);
   return (
     <nav id="sidebar" className="sidebar js-sidebar">
       <div className="sidebar-content js-simplebar">
